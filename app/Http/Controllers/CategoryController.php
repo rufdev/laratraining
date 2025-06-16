bd08d559f43d8c7edb2b6a828f53c03ba4a7353c
RUF.DEV <aguilarufino@gmail.com>
2025-06-16 19:50:04 +0800

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $message = "Hello from Server";
+        return inertia('Category/Index',[
+            'message' => $message
+        ]);
+    }
+}
---
+++ resources/js/pages/Category/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import AppLayout from '@/layouts/AppLayout.vue';
+    import { defineProps } from 'vue';
+    defineProps({
+        message: String
+    })
+</script>
+<template>
+    <AppLayout>
+        <h1>{{message}}</h1>
+    </AppLayout>
+</template>
