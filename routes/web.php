fb6234bcb55e670fd787cee2422a735ba566fd51
RUF.DEV <aguilarufino@gmail.com>
2025-06-17 09:34:58 +0800

    added assignment

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\AssetController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -13,6 +16,9 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('locations', [LocationController::class, 'index'])->name('locations');
+Route::get('manufacturers', [ManufacturerController::class, 'index'])->name('manufacturers');
+Route::get('assets', [AssetController::class, 'index'])->name('assets');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
