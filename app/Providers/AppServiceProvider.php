32bfc2c7a2f3f10b831d08821eff4c90d93129c3
RUF.DEV <aguilarufino@gmail.com>
2025-09-26 08:07:54 +0800

    not force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+        // if (env('APP_ENV') === 'production') {
+        //     URL::forceScheme('https');
+        // }
         
         Inertia::share([
             'auth' => function () {
